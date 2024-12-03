7889cd47094c811343629e90d35719ccdf4cd12e
avargas <alevargasgarran@gmail.com>
2024-12-03 10:51:30 +0100

    Ejercicio 2 Tailwind por terminar

---
+++ TailwindCSS/Ejercicio2/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./Ejercicio2/*.html",
+    "./Ejercicio2/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
